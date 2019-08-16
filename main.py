7b5807ca7038a2710cc386654c3685471e4af7d9
Vicky Thrasher <vic07kyt@gmail.com>
2019-08-16 17:02:59 -0400

    Randomly selects cluster centers

--- main.py
+++ main.py
@@ -23,8 +23,31 @@ def resizeImage(img, newMax):
     img = img.resize((newWidth, newHeight))
     return img
 
+def colorDistance(color1, color2):
+    dr = color2[0]-color1[0]
+    dg = color2[1]-color1[1]
+    db = color2[2]-color1[2]
+    dr2 = dr**2
+    dg2 = dg**2
+    db2 = db**2
+    return math.sqrt(dr2+dg2+db2)
+
+def getClusterCenters(width, height, k):
+    clusterCenters = []
+    for i in range(k):
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        if (x, y) not in clusterCenters:
+            clusterCenters.append((x, y))
+    return clusterCenters
+
+def clusterColors(img, k):
+    clusterCenters = getClusterCenters(img.size[0], img.size[1], k)
+
+
 if __name__ == "__main__":
     FILENAME = "./Images/silks.jpg"
     img = openImage(FILENAME)
     if img:
         img = resizeImage(img, 20)
+        img = clusterColors(img, 5)
