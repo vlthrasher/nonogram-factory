e5d4f40198330d6f3b6c74d1f73d00a2718d2343
Vicky Thrasher <vic07kyt@gmail.com>
2019-08-16 19:51:35 -0400

    Separated ColorClusterer into its own module

---
+++ ColorClusterer.py
@@ -0,0 +1,65 @@
+import math
+import random
+from statistics import mean
+
+class ColorClusterer:
+    def __init__(self):
+        pass
+
+    def clusterColors(self, img, k, iterations):
+        clusterCenters = self.__getClusterCenters(img, k)
+        for i in range(iterations):
+            clusters = self.__assignPixelsToCenters(img, clusterCenters)
+            clusterCenters = self.__recenterClusters(clusters, img)
+        clusters = self.__assignPixelsToCenters(img, clusterCenters)
+        print(clusters)
+        for k in clusters:
+            print(len(clusters[k]))
+        return clusters
+
+    def __getClusterCenters(self, img, k):
+        pixels = img.load()
+        clusterCenters = {}
+        for i in range(k):
+            x = random.randint(0, img.size[0] - 1)
+            y = random.randint(0, img.size[1] - 1)
+            if pixels[x, y] not in clusterCenters:
+                clusterCenters[pixels[x, y]] = []
+        return clusterCenters
+
+    def __colorDistance(self, color1, color2):
+        dr = color2[0] - color1[0]
+        dg = color2[1] - color1[1]
+        db = color2[2] - color1[2]
+        dr2 = dr ** 2
+        dg2 = dg ** 2
+        db2 = db ** 2
+        return math.sqrt(dr2 + dg2 + db2)
+
+    def __assignPixelsToCenters(self, img, clusterCenters):
+        pixels = img.load()
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                shortestDistance = 442
+                for k in clusterCenters:
+                    dist = self.__colorDistance(pixels[i, j], k)
+                    if dist < shortestDistance:
+                        cluster = k
+                        shortestDistance = dist
+                clusterCenters[cluster].append((i, j))
+        return clusterCenters
+
+    def __recenterClusters(self, clusters, img):
+        newClusters = {}
+        pixels = img.load()
+        for k in clusters:
+            r = []
+            g = []
+            b = []
+            for pixel in clusters[k]:
+                color = pixels[pixel[0], pixel[1]]
+                r.append(color[0])
+                g.append(color[1])
+                b.append(color[2])
+            newClusters[(mean(r), mean(g), mean(b))] = []
+        return newClusters
---
+++ ImageAdjuster.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import ColorClusterer
+
+
+class ImageAdjuster:
+    def __init__(self):
+        self.clusterer = ColorClusterer.ColorClusterer()
+
+    def __openImage(self, fileName):
+        try:
+            img = Image.open(fileName)
+        except IOError:
+            return
+        else:
+            return img
+
+    def __resizeImage(self, img, newMax):
+        width, height = img.size
+        if width > height:
+            newWidth = newMax
+            percent = float(newMax)/float(width)
+            newHeight = round(percent*height)
+        else:
+            newHeight = newMax
+            percent = float(newMax)/float(height)
+            newWidth = round(percent*width)
+        img = img.resize((newWidth, newHeight))
+        return img
+
+
+    def __recolorImage(self, img, clusters):
+        pixels = img.load()
+        for k in clusters:
+            for pixel in clusters[k]:
+                color = (round(k[0]), round(k[1]), round(k[2]))
+                pixels[pixel[0], pixel[1]] = color
+        return img
+
+    def adjustImage(self, fileName):
+        img = self.__openImage(fileName)
+        if img:
+            img = self.__resizeImage(img, 20)
+            clusters = self.clusterer.clusterColors(img, 7, 5)
+            img = self.__recolorImage(img, clusters)
+        return img
