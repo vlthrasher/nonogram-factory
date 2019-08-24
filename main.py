307f76dac10e44dfaa1afc90f93736fdcbef960c
Vicky Thrasher <vic07kyt@gmail.com>
2019-08-24 15:49:21 -0400

    Added Crayola50 Colorset

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import Colors
 if __name__ == "__main__":
     FILENAME = "./Images/silks.jpg"
     adjuster = ImageAdjuster.ImageAdjuster()
-    colorSet = Colors.CrayolaTwist30()
+    colorSet = Colors.Crayola50()
     img = adjuster.adjustImage(FILENAME, maxSize=20, clusterCount=5, iterations=5, colorSet=colorSet)
     img.show()
     displayer = Displayer.Displayer(img)
